397b430fee7e3fd4150daefe18e855c1f50b488d
Bruno Lemes <108091434+bruno-lemes@users.noreply.github.com>
2022-08-23 13:09:05 -0300

    Att "sistema"

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #pylint: disable=missing-module-docstring
+from tkinter.tix import Tree
 from nucleo_sistema.conta import ContaBanco
 
 
@@ -7,14 +8,11 @@ TITULO = 'BANCÃO DO BRUNÃO'
 print(f'{TITULO:^50}')
 print(50 * '=', '\n')
 
-
 nome = input('Nome do titular: ').title().strip()
-
 
 while True:
     tipo_da_conta = input('Tipo da conta\n1: Corrente (Bônus de saldo inicial: R$50)\n2:\
  Poupança (Bônus de saldo inicial: R$150)\nOpção: ').strip()
-
     match tipo_da_conta:
         case '1':
             TIPO_DA_CONTA = 'cc'
@@ -27,25 +25,30 @@ while True:
             print('\033[31mInforme um tipo válido de conta\033[m')
             print(50*'-')
 
-
 conta = ContaBanco()
-
 
 conta.abrir_conta(nome, TIPO_DA_CONTA)
 
 print('\033[32mConta aberta com sucesso!\033[m')
 
-
 while True:
     print(50 * '=')
     print('Escolha uma opção\n')
     print(' 1: Sacar \n 2: Depositar \n 3: Encerrar conta \n ')
-
     opcao = input('Opção: ').strip()
-
     match opcao:
         case '1':
-            break
+            while True:
+                try:
+                    saque = float(input('Valor do saque: '))
+                except ValueError:
+                    print('Informe um valor válido')
+                else:
+                    if conta.saldo_da_conta >= saque > 0:
+                        conta.sacar(saque)
+                        print('Saque realizado com sucesso.')
+                        break
+                    print('Informe um valor válido')
         case '2':
             break
         case '3':
